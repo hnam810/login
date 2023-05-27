c3720c5616f7d234bec3e793f106d650f1e386f0
Roy <roy@vinova.com.sg>
2023-05-27 11:46:54 +0700

    [UI] Login Form

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -1,14 +1,66 @@
-import React from 'react';
-import { Grid } from '@mui/material';
-import useStyles from './styles';
+import React from "react";
+import { Button, Checkbox, FormControlLabel, Grid, Link, TextField } from "@mui/material";
+import useStyles from "./styles";
+import ggIcon from "./icons/ggIcon.svg";
+import glIcon from "./icons/gitlabIcon.svg";
 
 const Login = () => {
-	const classes = useStyles();
-	return (
-		<Grid className={classes.root}>
-
-		</Grid>
-	);
+  const classes = useStyles();
+  return (
+    <Grid className={classes.root} display="flex">
+      <Grid className={classes.form}>
+        <img src={glIcon} alt="glIcon" className={classes.logo} />
+        <p className={classes.textTitle}>Welcome back</p>
+        <p className={classes.textSubTitle}>Welcome back! Please enter your details.</p>
+        <Grid maxWidth="360px" m="auto">
+          <Grid className={classes.input} display="flex" flexDirection="column">
+            <p>Email</p>
+            <TextField variant="outlined" classes={{ root: classes.inputRoot }} placeholder="Enter your email" />
+            <p>Password</p>
+            <TextField
+              variant="outlined"
+              type="password"
+              classes={{ root: classes.inputRoot }}
+              placeholder="Enter your password"
+            />
+          </Grid>
+          <Grid className={classes.checkBox} display="flex" alignItems="center" justifyContent="space-between">
+            <FormControlLabel control={<Checkbox size="small" />} label="Remember for 30 days" />
+            <Link href="">Forgot password</Link>
+          </Grid>
+          <Grid className={classes.btn}>
+            <Button variant="contained" fullWidth sx={{ background: "#7F56D9", border: "1px solid #7F56D9" }}>
+              Sign in
+            </Button>
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{ background: "#FFF", border: "1px solid #D0D5DD", color: "#000" }}
+            >
+              <img src={ggIcon} alt="ggIcon" />
+              Sign in with Google
+            </Button>
+          </Grid>
+          <p className={classes.textForgot}>
+            Don’t have an account? <Link href="">Forgot password</Link>
+          </p>
+        </Grid>
+      </Grid>
+      <Grid className={classes.bkg} display="flex" flexDirection="column" justifyContent="flex-end">
+        <Grid className={classes.info}>
+          <p className="title">
+            “Untitled has saved us thousands of hours of work. We’re able to spin up projects faster and take on more
+            clients.”
+          </p>
+          <Grid>
+            <p className="textName">Lula Meyers</p>
+            <p className="textBy">Product Manager, Hourglass</p>
+            <p className="textTy">Web Design Agency</p>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Login;
--- src/modules/Login/styles.tsx
+++ src/modules/Login/styles.tsx
@@ -1,11 +1,184 @@
 import { createStyles, makeStyles } from "@mui/styles";
+import bkg from "./icons/bkg.jpg";
 
 export default makeStyles((theme) =>
-	createStyles({
-		root:{
-			
-		}
-	})
+  createStyles({
+    root: {
+      height: "100vh",
+    },
+    form: {
+      width: "50%",
+      margin: "auto",
+      "@media only screen and (max-width: 960px)": {
+        width: "100%",
+        margin: "auto 15px",
+      },
+    },
+    bkg: {
+      backgroundImage: `url(${bkg})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "right",
+      width: "50%",
+      margin: 24,
+      borderRadius: 20,
+      "@media only screen and (max-width: 960px)": {
+        display: "none !important",
+      },
+    },
+    input: {
+      "& p": {
+        fontStyle: "normal",
+        fontWeight: 500,
+        fontSize: "14px",
+        lineHeight: "20px",
+        color: "#344054",
+        marginBottom: 6,
+        marginTop: 20,
+      },
+    },
+    logo: {
+      width: 30,
+      margin: "auto",
+      display: "block",
+    },
+    textTitle: {
+      fontStyle: "normal",
+      fontWeight: 600,
+      fontSize: "30px",
+      lineHeight: "38px",
+      marginBottom: 12,
+      marginTop: 24,
+      textAlign: "center",
+      color: "#101828",
+    },
+    textSubTitle: {
+      fontStyle: "normal",
+      fontWeight: 400,
+      fontSize: "16px",
+      lineHeight: "24px",
+      color: "#667085",
+      marginBottom: 12,
+      textAlign: "center",
+    },
+    inputRoot: {
+      background: "#FFFFFF",
+      "& input": {
+        padding: "10px 14px",
+        fontStyle: "normal",
+        fontWeight: 400,
+        fontSize: "16px",
+        lineHeight: "24px",
+      },
+      "& fieldset": {
+        border: "1px solid #D0D5DD !important",
+        boxShadow: "0px 1px 2px rgba(16, 24, 40, 0.05)",
+        borderRadius: "8px",
+      },
+    },
+    checkBox: {
+      marginTop: 15,
+      marginBottom: 15,
+      "& span": {
+        fontStyle: "normal",
+        fontWeight: 500,
+        fontSize: "14px",
+        lineHeight: "20px",
+        color: "#344054",
+      },
+      "& a": {
+        fontStyle: "normal",
+        fontWeight: 600,
+        fontSize: "14px",
+        lineHeight: "20px",
+        color: "#6941C6",
+        textDecoration: "unset",
+      },
+    },
+    btn: {
+      "& button": {
+        boxShadow: "0px 1px 2px rgba(16, 24, 40, 0.05)",
+        borderRadius: "8px",
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: "16px",
+        lineHeight: "24px",
+        textTransform: "none",
+        padding: "10px 0",
+        marginBottom: 16,
+        "&:hover": {
+          backgroundColor: "#bbadd3b8",
+          boxShadow: "none",
+        },
+        "& img": {
+          width: 24,
+          paddingRight: 12,
+        },
+      },
+    },
+    info: {
+      height: "322px",
+      background: "rgba(255, 255, 255, 0.3)",
+      borderTop: "1px solid rgba(255, 255, 255, 0.5)",
+      backdropFilter: "blur(12px)",
+      borderRadius: '0px 0px 20px 20px',
+      "& .title": {
+        fontStyle: "normal",
+        fontWeight: 600,
+        fontSize: "30px",
+        lineHeight: "30px",
+        color: "#000",
+        padding: "32px",
+        margin: 0,
+      },
+      "& .textName": {
+        fontStyle: "normal",
+        fontWeight: 600,
+        fontSize: "36px",
+        lineHeight: "44px",
+        color: "#000",
+        paddingBottom: "12px",
+        paddingLeft: 32,
+        margin: 0,
+      },
+      "& .textBy": {
+        fontStyle: "normal",
+        fontWeight: 600,
+        fontSize: "18px",
+        lineHeight: "28px",
+        color: "#000",
+        paddingBottom: "12px",
+        paddingLeft: 32,
+        margin: 0,
+      },
+      "& .textTy": {
+        fontStyle: "normal",
+        fontWeight: 500,
+        fontSize: "16px",
+        lineHeight: "24px",
+        color: "#000",
+        paddingBottom: "4px",
+        paddingLeft: 32,
+        margin: 0,
+      },
+    },
+    textForgot: {
+      textAlign: "center",
+      fontStyle: "normal",
+      fontWeight: 400,
+      fontSize: "14px",
+      lineHeight: "20px",
+      color: "#475467",
+      "& a": {
+        fontStyle: "normal",
+        fontWeight: 600,
+        fontSize: "14px",
+        lineHeight: "20px",
+        color: "#6941C6",
+        textDecoration: "unset",
+      },
+    },
+  })
 );
 
 // export default useStyles;
