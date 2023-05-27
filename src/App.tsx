9082e5e015cf51536fc2d852d04b34a62cb6a767
Roy <roy@vinova.com.sg>
2023-05-27 08:35:16 +0700

    New project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+
+const App = () => {
+  const AppContainer = styled.div`
+    height: 100%;
+  `;
+  return <AppContainer>
+    
+  </AppContainer>;
+};
+
+export default App;
